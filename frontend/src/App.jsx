1d995d7a71bfa22613d32ac7b7f2bb44858a319e
Riya <riyabpatel1114@gmail.com>
2025-05-12 12:12:08 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Doctor from './pages/Doctor'
 import Login from './pages/Login'
 import About from './pages/About'
